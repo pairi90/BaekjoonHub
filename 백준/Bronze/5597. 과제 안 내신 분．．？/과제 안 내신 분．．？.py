8d93c7d25330941101ed31bf462350e610c3c50e
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-03-27 18:42:08 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,8 @@
+li = []
+for _ in range(1, 31):
+    li.append(_) #학생 명부
+
+for _ in range(1, 29): # 과제를 제출한 학생수
+    n = int(input()) # 과제 제출 학생의 출석번호 
+    li.remove(n) # 제거
+print(*sorted(li)) # 출석번호 순서대로 보여주기
